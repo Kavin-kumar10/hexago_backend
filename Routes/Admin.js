8f9dd7bcb3a8689e60e95a9ec45c308e6c24d2ce
kavin-kumar10 <kavinkumar.prof@gmail.com>
2023-09-23 03:06:36 +0530

    Routes admin

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -8,6 +8,23 @@ router.get('/',async(req,res)=>{
     res.send(response);
 })
 
+router.post('/SignIn',async(req,res)=>{
+    const {email,password} = req.body;
+    const Users = await Admin.findOne({email,password});
+    if(Users){
+        res.status(200).send({
+            success:true,
+            data:Users
+        })
+    }
+    else{
+        res.status(401).send({
+            success:false,
+            data:"Account not found"
+        })
+    }
+})
+
 router.post('/',async(req,res)=>{
 
     const {name,password,email,lat,lon,city} = req.body;
